b465c9ecbb37cb1b178b9179918c4a03cabb7c67
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-06 09:38:55 +0100

    init + pages

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/App.css';
+
+export default function App() {
+  console.log('App susikure');
+  return (
+    <div className='App container'>
+      <h1>Hello, world!</h1>
+      <hr />
+    </div>
+  );
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function HomePage() {
+
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem fugiat in, placeat illum eos dicta quae ab ipsum fugit nobis.</p>
+      <button>Read more</button>
+      </div>
+  )
+}
